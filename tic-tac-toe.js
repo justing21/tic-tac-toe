0d1a3660973551dbb091ffd0385c81a36433eb21
Justin  Gutierrez <justin.g21@icloud.com>
2016-11-16 21:02:42 -0800

    new everything

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,10 +3,40 @@
 var namespace = "http://www.w3.org/2000/svg"
 function blank(){
   var circle = document.createElementNS(namespace, "circle")
-  circle.setAttribute("cx", 50)
-  circle.setAttribute("cy", 50)
-  circle.setAttribute("r", 10)
+  circle.setAttribute("cx", 45)
+  circle.setAttribute("cy", 45)
+  circle.setAttribute("r", 30)
   circle.setAttribute("fill", "white")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(circle)
+}
+
+function blank2(){
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", 145)
+  circle.setAttribute("cy", 145)
+  circle.setAttribute("r", 30)
+  circle.setAttribute("fill", "white")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+}
+function blank3(){
+  var rectangle = document.createElementNS(namespace, "rect")
+  rectangle.setAttribute("x", 10)
+  rectangle.setAttribute("y", 110)
+  rectangle.setAttribute("height", 70)
+  rectangle.setAttribute("width", 70)
+  rectangle.setAttribute("fill", "blue")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(rectangle)
+}
+function blank4(){
+  var rectangle = document.createElementNS(namespace, "rect")
+  rectangle.setAttribute("x", 210)
+  rectangle.setAttribute("y", 210)
+  rectangle.setAttribute("height", 70)
+  rectangle.setAttribute("width", 70)
+  rectangle.setAttribute("fill", "blue")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(rectangle)
 }
