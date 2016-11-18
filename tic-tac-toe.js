dd4bbe39db1a7cbaafacbf522ca92ee21385c10d
Justin  Gutierrez <justin.g21@icloud.com>
2016-11-18 10:17:15 -0800

    ieufgyi

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -15,15 +15,14 @@ var namespace = "http://www.w3.org/2000/svg"
     turn="player2"
   }else{
     var circle = document.createElementNS(namespace, "circle")
-    circle.setAttribute("cx", 10)
+    circle.setAttribute("cx", 110)
     circle.setAttribute("cy", 10)
-    circle.setAttribute("r", 70)
-    circle.setAttribute("fill", "orange")
+    circle.setAttribute("r", 70)circle.setAttribute("fill", "orange")
     var canvas = document.getElementById("game-board")
     canvas.appendChild(circle)
     turn="player1"
  }
-
+}
     var rectangle = document.createElementNS(namespace, "rect")
     rectangle.setAttribute("x", 10)
     rectangle.setAttribute("y", 10)
@@ -32,8 +31,15 @@ var namespace = "http://www.w3.org/2000/svg"
     rectangle.setAttribute("fill", "white")
     var canvas = document.getElementById("game-board")
     canvas.appendChild(rectangle)
-}
+  }
 function blank2(){
+  var circle = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", 145)
+  circle.setAttribute("cy", 145)
+  circle.setAttribute("r", 30)
+  circle.setAttribute("fill", "white")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
     var rectangle = document.createElementNS(namespace, "rect")
     rectangle.setAttribute("x", 110)
     rectangle.setAttribute("y", 10)
@@ -44,6 +50,23 @@ function blank2(){
     canvas.appendChild(rectangle)
 }
 function blank3(){
+  var rectangle = document.createElementNS(namespace, "rect")
+  rectangle.setAttribute("x", 10)
+  rectangle.setAttribute("y", 110)
+  rectangle.setAttribute("height", 70)
+  rectangle.setAttribute("width", 70)
+  rectangle.setAttribute("fill", "blue")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(rectangle)
+}
+function blank4(){
+  var rectangle = document.createElementNS(namespace, "rect")
+  rectangle.setAttribute("x", 210)
+  rectangle.setAttribute("y", 210)
+  rectangle.setAttribute("height", 70)rectangle.setAttribute("width", 70)
+  rectangle.setAttribute("fill", "blue")
+  var canvas = document.getElementById("game-board")
+  canvas.appendChild(rectangle)
     var rectangle = document.createElementNS(namespace, "rect")
     rectangle.setAttribute("x", 210)
     rectangle.setAttribute("y", 10)
